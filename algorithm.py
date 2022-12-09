947ad34b9d5d021da80de9c3c6ecf00f02dbf5b3
ymh3190 <ymh3190@gmail.com>
2022-12-09 11:43:18 +0900

    12.9

--- algorithm.py
+++ algorithm.py
@@ -1,21 +1,35 @@
-from classification import Classification
-from regression import Regression
-from unsupervised_learning import UnsupervisedLearning
+def classify() -> None:
+    from classification import Classification
+    Classification.classify()
+    Classification.classify_logistic_regression()
+    Classification.classify_stochastic_gradient_descent()
+    Classification.classify_decision_tree()
+    Classification.classify_cross_validation()
+    Classification.classify_tree_ensemble()
+    Classification.classify_gradient_boosting()
+    Classification.classify_histogram_based_gradient_boosting()
+
+
+def regress() -> None:
+    from regression import Regression
+    Regression.regress()
+    Regression.regress_linear()
+    Regression.regress_linear_multiple()
+
+
+def cluster() -> None:
+    from cluster import Cluster
+    Cluster.cluster()
+    Cluster.cluster_center()
+    Cluster.dimension_reduction()
 
 
 def algorithm() -> None:
-    # Classification.classify()
-    # Classification.logistic_regression()
-    # Regression.regress()
-    # Regression.regress_linear()
-    # Regression.regress_linear_multiple()
-    # Classification.stochastic_gradient_descent()
-    # Classification.classify_decision_tree()
-    # Classification.classify_cross_validation()
-    # Classification.classify_tree_ensemble()
-    # Classification.classify_gradient_boosting()
-    # Classification.classify_histogram_based_gradient_boosting()
+    # classify()
+    # regress()
+    # cluster()
+    tmp()
 
-    # UnsupervisedLearning.cluster()
-    # UnsupervisedLearning.cluster_center()
-    UnsupervisedLearning.dimension_reduction()
+
+def tmp() -> None:
+    pass
--- cluster.py
+++ cluster.py
@@ -1,4 +1,4 @@
-class UnsupervisedLearning():
+class Cluster():
     def cluster() -> None:
         """ 비지도학습이지만 어떤 사진인지 알고있었다. """
         import os
@@ -120,6 +120,7 @@ class UnsupervisedLearning():
         fruits = np.load(FRUITS_300_npy)
         fruits_2d = fruits.reshape(-1, 100*100)
         from sklearn.decomposition import PCA
+        """ 주성분 개수 지정 """
         pca = PCA(n_components=50)
         pca.fit(fruits_2d)
         # print(pca.components_.shape)
@@ -133,7 +134,46 @@ class UnsupervisedLearning():
 
         fruits_reconstruct = fruits_inverse.reshape(-1, 100, 100)
         for start in [0, 100, 200]:
-            draw_fruits(fruits_reconstruct[start:start+100])
+            # draw_fruits(fruits_reconstruct[start:start+100])
+            continue
+        # print(np.sum(pca.explained_variance_ratio_))
+        import matplotlib.pyplot as plt
+        # plt.plot(pca.explained_variance_ratio_)
+        # plt.show()
+
+        from sklearn.linear_model import LogisticRegression
+        lr = LogisticRegression()
+        target = np.array([0]*100+[1]*100+[1]*100)
+        from sklearn.model_selection import cross_validate
+        scores = cross_validate(lr, fruits_2d, target)
+        print(np.mean(scores['test_score']))
+        print(np.mean(scores['fit_time']))
+        scores = cross_validate(lr, fruits_pca, target)
+        print(np.mean(scores['test_score']))
+        print(np.mean(scores['fit_time']))
+
+        """ 설명된 분산 비율 입력 """
+        pca = PCA(n_components=0.5)
+        pca.fit(fruits_2d)
+        print(pca.n_components_)
+        fruits_pca = pca.transform(fruits_2d)
+        print(fruits_pca.shape)
+        scores = cross_validate(lr, fruits_pca, target)
+        print(np.mean(scores['test_score']))
+        print(np.mean(scores['fit_time']))
+
+        from sklearn.cluster import KMeans
+        km = KMeans(n_clusters=3, random_state=42)
+        km.fit(fruits_pca)
+        # print(np.unique(km.labels_,return_counts=True))
+        for label in range(3):
+            draw_fruits(fruits[km.labels_ == label])
+
+        for label in range(3):
+            data = fruits_pca[km.labels_ == label]
+            plt.scatter(data[:, 0], data[:, 1])
+        plt.legend(['apple', 'banana', 'pineapple'])
+        plt.show()
 
 
 def draw_fruits(arr, ratio=1):
